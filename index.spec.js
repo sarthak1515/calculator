0eda3dfa683c789e65c35afe4afe6542ae2ef81e
Sarthak <sarthak.chaurasia@triconinfotech.com>
2022-06-28 18:01:53 +0530

    first commit

---
+++ index.spec.js
@@ -0,0 +1,12 @@
+const add=require("../backend/index")
+const assert=require("assert")
+try{
+    var sum=7;
+    assert.equal(sum,add(3,4))
+    console.log("test case success")
+}
+catch(e)
+{
+ console.log(e);
+}
+
